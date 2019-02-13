96f4b9f7a007a29043801196b432dce8ab319b0b
aguerrero232 <47163978+aguerrero232@users.noreply.github.com>
2019-02-12 20:25:58 -0600

    Add files via upload

---
+++ Conversion2_AIG/Conversion2_AIG/Conversion2_AIG.cpp
@@ -0,0 +1,41 @@
+// Ariel Guerrero
+// 2/8/2019
+// conversion_AGuerrero.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+/*
+
+ Meat of the program that prompts the user on screen to enter the amount of miles to convert and
+ multiplies it by the conversion factor to get the Kilometers that equal to the same amount of
+ miles provided by the user.
+
+*/
+int main()
+{
+	// constant factor used to convert miles to km	
+	double CONVERSIONFACTOR = 1.609;
+
+	// variables that change: 
+	double km = 0;
+	double mi = 0;
+
+	// prompts the user to do what we want them to do	
+	cout << "Enter the Miles(mi) you would like to convert to Kilometers(km): ";
+
+	// takes in the user input and stores it the variable for miles
+	cin >> mi;
+
+	// sets our variable for kilometers to miles * the conversion factor
+	km = mi * CONVERSIONFACTOR;
+
+	// display our results for the user to see on screen
+	cout << "There are "<< setw(10) << setfill('*') << setprecision(3) << km;
+	cout << setprecision(3) << setw(50) << setfill('$') << "km in " << mi << "mi."<< endl;
+
+}
+
