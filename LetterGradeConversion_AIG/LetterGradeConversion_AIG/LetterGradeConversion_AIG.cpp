fed9776ea8d6525826358df7c5ecd79db1e6e36b
Ariel Guerrero <47163978+aguerrero232@users.noreply.github.com>
2019-03-11 15:54:27 -0500

    Add files via upload

--- LetterGradeConversion_AIG/LetterGradeConversion_AIG/LetterGradeConversion_AIG.cpp
+++ LetterGradeConversion_AIG/LetterGradeConversion_AIG/LetterGradeConversion_AIG.cpp
@@ -2,52 +2,48 @@
 // 3/5/2019
 //
 // Outputs the letter Grade equivalent to the user inputted numerical grade
+// UPDATED 3/11/2019 checks to round the students grade 
 // 
 
 #include "pch.h"
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
+
 /*
 	Name: Main
-	Parameters: 
+	Parameters:
 	Description: takes a number value and calculated your letter grade given a value between 0-100
-
 */
 int main()
 {
 
 	// varible used to hold the users grade
 	double dblGrade = 0.0;
+	double dblRoundCheck = 0.0;
 
 	//  variable used to get the case for the switch statement 
 	int intSwitchGradeCase = 0;
-
+	int intGradeHolder = 0;
 
 	// prompt the user fortheir grade value 
 	cout << "Input your number Grade: ";
 	cin >> dblGrade;
 
-/*
-		IN CLASS WORK USING IF LOGIC
-	if (dblGrade > 89) {
-		cout << "You have an A!" << endl;
-	}
-	if (dblGrade < 90 && intGrade > 79) {
-		cout << "You have an B!" << endl;
-	}
-	if (dblGrade < 80 && intGrade > 69) {
-		cout << "You have an C!" << endl;
-	}
-	if (dblGrade <= 70 && intGrade > 59) {
-		cout << "You have an D!" << endl;
-	}
-	if (dblGrade < 60) {
-		cout << "You have an F!" << endl;
-	}
-*/
 	// calculates the switch case and stores it as an int 
+	intGradeHolder = dblGrade;
 	intSwitchGradeCase = dblGrade / 10;
+	dblRoundCheck = intGradeHolder % 10;
+
+	//cout << fixed << setprecision(2) << "CHECK intGrade%10: " << dblRoundCheck << endl;
+	//cout << fixed << setprecision(2) << "CHECK int: " << intSwitchGradeCase << endl;
+	//cout << fixed << setprecision(2) << "CHECK math: " << intSwitchGradeCase << endl;
+
+	if (dblRoundCheck >= 9) {
+		intSwitchGradeCase++;
+		cout << fixed << setprecision(2) << "LUCKY YOU!!!" << intSwitchGradeCase << endl;
+	}
 
 	// switch statement to print the users certain letter grade
 	switch (intSwitchGradeCase) {
@@ -67,7 +63,7 @@ int main()
 	case 6:
 		cout << "You have an D!" << endl;
 		break;
-	default: 
+	default:
 		cout << "YOU GOT AN F!" << endl;
 	}
 }
