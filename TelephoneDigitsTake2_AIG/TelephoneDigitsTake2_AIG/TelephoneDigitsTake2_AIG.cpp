3f101978ae741a9d286bf6d7fa0bffa67a7afab1
Ariel Guerrero <47163978+aguerrero232@users.noreply.github.com>
2019-03-28 17:13:43 -0500

    Add files via upload

--- TelephoneDigitsTake2_AIG/TelephoneDigitsTake2_AIG/TelephoneDigitsTake2_AIG.cpp
+++ TelephoneDigitsTake2_AIG/TelephoneDigitsTake2_AIG/TelephoneDigitsTake2_AIG.cpp
@@ -1,79 +1,97 @@
-// TelephoneDigitsTake2_AIG.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 #include "pch.h"
 #include <iostream>
 #include <string>
 using namespace std;
+/*
+name: main
+parameters: n/a
+description: gets the users input and spits out a phone number corresponding to the letter or number or character they pushed
 
+
+*/
 int main()
 {
 	string strUsrInputString = "DEFAULT";
 	string strPhoneNumber = "";
+	string strLeftover = "";
+	
 	char chrCurrentCharacterHolder = ' ';
 	char chrUserRepeatOption = 'y';
+	int intLoopCounter = 0;
 	do {
+		// just incase anything from last run is left over i just reset everything to make sure im starting off clean
+		strUsrInputString = "DEFAULT";
+		strPhoneNumber = "";
 		cout << "Enter Letters corresponding to a phone number: " << endl;
-		getline(cin,strUsrInputString);
-		// make sure the user is using the right length 
-		if (strUsrInputString.size() != 7) {
-			cout << "Enter Letters corresponding to a phone number: " << endl;
-			cout << "EXACTLY 7 LETTERS ARE NEEDED! " << endl;
-			getline(cin,strUsrInputString);
-		}
-		// just need it to go through for 7 digits after that I dont care 
-		for (unsigned int charCheck = 0; charCheck < 7; charCheck++) {
-			chrCurrentCharacterHolder = strUsrInputString.at(charCheck);
-			cout << "Current Char: " << chrCurrentCharacterHolder << endl;
-			//																									a-z
+		do {
+			cin >> chrCurrentCharacterHolder;  // <-- how is this working if i can hit backspace and type multiple before hitting enter?
+			// cout << "YOU NEED " << 10 - strPhoneNumber.size() << " MORE CHARACTERS!" << endl; // only want this to display once per enter 
 			// only converting characters to uppercase if they are a lowercase letter in between 96 and 123 so 97-122
 			if (((int)chrCurrentCharacterHolder > 96) && ((int)chrCurrentCharacterHolder < 123)) {
 				chrCurrentCharacterHolder = toupper(chrCurrentCharacterHolder);
 			}
 			// magic happens here
 			switch (chrCurrentCharacterHolder) {
-			case ' ':
-				strPhoneNumber += "1"; // needs more work here and for the last case 
+			case '1':
+				strPhoneNumber += "1";
 				break;
-			case 'A': case 'B': case 'C':
+			case '2': case 'A': case 'B': case 'C':
 				strPhoneNumber += "2";
 				break;
-			case 'D': case 'E': case 'F':
+			case '3': case 'D': case 'E': case 'F':
 				strPhoneNumber += "3";
 				break;
-			case 'G': case 'H': case 'I':
+			case '4': case 'G': case 'H': case 'I':
 				strPhoneNumber += "4";
 				break;
-			case 'J': case 'K': case 'L':
+			case '5': case 'J': case 'K': case 'L':
 				strPhoneNumber += "5";
 				break;
-			case 'M': case 'N': case 'O':
+			case'6':case 'M': case 'N': case 'O':
 				strPhoneNumber += "6";
 				break;
-			case 'P': case 'Q': case 'R': case 'S':
+			case '7': case 'P': case 'Q': case 'R': case 'S':
 				strPhoneNumber += "7";
 				break;
-			case 'T': case 'U': case 'V':
+			case '8': case 'T': case 'U': case 'V':
 				strPhoneNumber += "8";
 				break;
-			case 'W': case 'X': case 'Y': case 'Z':
+			case '9': case 'W': case 'X': case 'Y': case 'Z':
 				strPhoneNumber += "9";
 				break;
-			case '+':
-				strPhoneNumber += "0"; // couldnt get this to work... hmmm?
+			case '0':
+				strPhoneNumber += "0";
+				break;
+			case '*':
+				strPhoneNumber += "*";
+				break;
+			case '#':
+				strPhoneNumber += "#";
 				break;
 			default:
-				cout << "THIS IS NOT A LETTER!" << endl;
-				cout << "AN \"X\" WILL BE SHOWN IN THE PHONE NUMBER IN PLACE OF THE INCORRECT INPUT!" << endl;
-				strPhoneNumber += "X";
+				cout << "ERROR "<< chrCurrentCharacterHolder <<" IS NOT A CHARACTER ON THE PHONE!" << endl;
 			}
-		}
-		// taking pride in my work and making things look good-ish	
-		strPhoneNumber = strPhoneNumber.substr(0, 3) + "-" + strPhoneNumber.substr(3, 4);
-		cout << "Your phone number produced is: " << strPhoneNumber << endl;
+			
+			chrCurrentCharacterHolder = ' ';
+			intLoopCounter++;
+		} while (strPhoneNumber.size() < 10);
+
+			strPhoneNumber = "(" + strPhoneNumber.substr(0, 3) + ")" + strPhoneNumber.substr(3, 3) + "-" + strPhoneNumber.substr(6, 4);
+			cout << "Your phone number produced is: " << strPhoneNumber << endl;
+
+			// had to do this just incase they had used more than 10 chars 
+		cin >> strLeftover;
+		strLeftover = ""; // poof and now its gone
+		// restore for next run!
 		strPhoneNumber = "";
-		// easier for testing so i dont have to keep having to reset 
+	
+		// easier for testing so i dont have to keep reseting  
 		cout << "Would you like to repeat this process? (y for yes, n for no): " << endl;
 		cin >> chrUserRepeatOption;
+		
+		chrUserRepeatOption = tolower(chrUserRepeatOption);
+
 	} while (chrUserRepeatOption == 'y');
 	return 0;
 }
